df7a3d37fa723a6055664eb6a9af558483acd302
Cherise <cnh3qa@virginia.edu>
2018-04-14 16:33:50 -0400

    add DB login
    add database functionality for logging in by adding php files for creating a new user and checking against existing user credentials for logging in

---
+++ CreateUser.php
@@ -0,0 +1,34 @@
+<?php
+include_once("./library.php"); // To connect to the database
+session_start();
+$_SESSION["user"] = $_POST["username"];
+$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+// Check connection
+if (mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  $sqlAllUsers = "SELECT user_id FROM User";
+  $result = mysqli_query($con,$sqlAllUsers);
+
+// Form the SQL query (an INSERT query)
+   $sql="INSERT INTO User (user_id, username, password, location, rating)
+   VALUES
+   (" . mysqli_num_rows($result) . ", '$_POST[username]','$_POST[password]','$_POST[location]', 'N/A')";
+if (!mysqli_query($con,$sql))
+  {
+    die('Error: ' . mysqli_error($con));
+  }
+
+  
+redirect('http://localhost:8080/database_project/homepage.php');
+mysqli_close($con);
+
+function redirect($url) {
+    ob_start();
+    header('Location: '.$url);
+    ob_end_flush();
+    die();
+}
+?>
